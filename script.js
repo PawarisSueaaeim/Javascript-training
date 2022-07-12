525fbebe1886635ef77e5f2e000a211d607972dc
PawarisSueaaeim <97650022+PawarisSueaaeim@users.noreply.github.com>
2022-07-12 11:54:54 +0700

    update

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const fname_lastname = document.querySelector("#name");
-var add = document.querySelector("#submit");
-var male = document.querySelector("#male");
+const add = document.querySelector("#submit");
+const male = document.querySelector("#male");
 const female = document.querySelector("#female");
-
+var accept = document.getElementById("accept");
+var arr = [];
+count = 0;
 add.focus();
 add.onclick = function () {
+  count += 1;
+  if (accept.checked == true) {
     var text = fname_lastname.value;
     var gender = document.querySelector(
       'input[name="flexRadioDefault"]:checked'
@@ -14,12 +18,21 @@ add.onclick = function () {
       name: fname_lastname.value,
       gender: gender,
     };
+    for (let i = 0; i < count; i++) {
+      arr[i] = data;
+    }
+    console.log(arr);
 
     if (text == "") {
       alert("Please enter a username");
     } else {
+      for (let i = 0; i < arr.length; i++) {}
+
       document.createElement("tr").innerHTML;
       document.getElementById("dataname").innerHTML = data.name;
       document.getElementById("datagender").innerHTML = data.gender;
     }
-  };
+  } else {
+    alert("Please accept");
+  }
+};
